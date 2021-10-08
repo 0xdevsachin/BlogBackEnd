4139b2a86c0cd21689cf8891902328015bc271e3
sachin320 <sachinvermavs786@gmail.com>
2021-10-08 16:19:42 +0530

    Front-end connected

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 3300;
+
+
+app.use(cors());
+
+
+app.post('/api', (req, res) => {
+    res.send("Api Call Recieved")
+    console.log("React Request ")
+})
+
+app.listen(PORT, () => {
+    console.log('Server started at http://localhost:3300')
+})
