b66781949b654b34fc8439374e96f58d19d706ac
sachin320 <sachinvermavs786@gmail.com>
2021-10-08 20:37:57 +0530

    Express Routes Added

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const PORT = process.env.PORT || 3300;
+var signup = require('./routes/signup')
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
-app.use(cors());
-
-
-app.post('/api', (req, res) => {
-    console.log(req.body);
-    res.send("Api Call Recieved")
-    console.log("React Request ")
-})
-
-app.listen(PORT, () => {
+app.listen(3300, () => {
     console.log('Server started at http://localhost:3300')
 })
+
+
+app.use('/api', signup);
