222018c4a2a310a9eaf74ff2ece495d295e81c77
sachin320 <sachinvermavs786@gmail.com>
2021-10-09 00:08:12 +0530

    Registration Schema Complete

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    cpassword: {
+        type: String,
+        require: true,
+    }
+})
+
+const user = mongoose.model('user', userSchema);
+
+module.exports = user;
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,29 @@
 var express = require('express')
 var router = express.Router();
+const { findOne } = require('../models/user');
+var userSchema = require('../models/user')
 
+router.post('/auth/signup', async function(req, res) {
+    var email = await userSchema.findOne({ email: req.body.email })
+    var user = await userSchema.findOne({ username: req.body.username })
+    if (user) {
+        res.send({ msg: "Username Already Taken", redirect: false })
+        console.log("user is already taken")
+    } else if (email) {
+        res.send({ msg: "Email Already Taken", redirect: false })
+        console.log("email is already taken")
+    } else if (req.body.password !== req.body.cpassword) {
+        res.send("Password and Confirm Password must be same")
+    } else {
+        res.send({ msg: "Account Registered Succesfully", redirect: true })
+        const saveData = new userSchema(req.body);
+        saveData.save().then(() => {
+            console.log(saveData);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
 
-router.post('/auth/signup', function(req, res) {
-    console.log(req.body);
 })
 
 module.exports = router
