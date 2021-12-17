3c36d5ad6d8e28b284276f2c88bbb147307b7246
sachin320 <sachinvermavs786@gmail.com>
2021-12-17 23:42:48 +0530

    Sign up, Sign in Route Updated

--- routes/signin.js
+++ routes/signin.js
@@ -1,38 +1,39 @@
-var express = require('express')
-var router = express.Router()
-var userSchema = require('../models/user')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+var express = require("express");
+var router = express.Router();
+var userSchema = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+router.post("/auth/signin", async function (req, res) {
+  var user = await userSchema
+    .findOne({ username: req.body.username })
+    .catch((err) => {
+      console.log(err);
+    });
 
-router.post('/auth/signin', async function(req, res) {
-    var user = await userSchema.findOne({ username: req.body.username }).catch((err) => {
-        console.log(err)
-    })
-
-    if (req.body.username === '' || req.body.password === '') {
-        res.send({ msg: "Input Field can't be Empty ", success: false })
-    }
-    if (user) {
-        var pass = user.password
-        // comapring using Bcrypt JS
-        const password = await bcrypt.compare(req.body.password, pass)
-        if (!password) {
-            res.status(401).send({ msg: "Invalid Credentials", success: false })
-        }
-        else{
-            const data = {
-                user : user
-            }
-            // set Expiration time in JWT Token (Default : 5 minutes)
-            const authtoken = jwt.sign(data, process.env.JWT_SECRET)
-            res.status(200).send({ msg: "Login Succesfully", authtoken, success: true })
-        }
+  if (req.body.username === "" || req.body.password === "") {
+    res.send({ msg: "Input Field can't be Empty ", success: false });
+  }
+  if (user) {
+    var pass = user.password;
+    // comapring using Bcrypt JS
+    const password = await bcrypt.compare(req.body.password, pass);
+    if (!password) {
+      res.status(200).send({ msg: "Invalid Credentials", success: false });
     } else {
-        res.status(401).send({ msg: "Invalid Credentials", success: false })
-        console.log("User not exist ")
+      const data = {
+        user: user,
+      };
+      // set Expiration time in JWT Token (Default : 5 minutes)
+      const authtoken = jwt.sign(data, process.env.JWT_SECRET);
+      res
+        .status(200)
+        .send({ msg: "Login Succesfully", authtoken, success: true });
     }
+  } else {
+    res.status(200).send({ msg: "Invalid Credentials", success: false });
+    console.log("User not exist ");
+  }
+});
 
-})
-
-module.exports = router
+module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,46 +1,58 @@
-var express = require('express')
+var express = require("express");
 var router = express.Router();
-var userSchema = require('../models/user')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+var userSchema = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/auth/signup', async function(req, res) {
-    console.log(req.body);
-    var email = await userSchema.findOne({ email: req.body.email })
-    var user = await userSchema.findOne({ username: req.body.username })
-    if (req.body.username == '' || req.body.email == '' || req.body.password == '' || req.body.cpassword == '') {
-        res.send({ msg: "Something Went Wrong ", success: true })
+router.post("/auth/signup", async function (req, res) {
+  console.log(req.body);
+  var email = await userSchema.findOne({ email: req.body.email });
+  var user = await userSchema.findOne({ username: req.body.username });
+  if (user) {
+    res.send({ msg: "Username Already Exist", sucess: false });
+    console.log("user is already exist");
+  } else if (email) {
+    res.send({ msg: "Email Already Exist", success: false });
+    console.log("email is already exist");
+  } else if (req.body.password !== req.body.cpassword) {
+    res.send("Password and Confirm Password must be same");
+  } else if (
+    req.body.username == "" ||
+    req.body.email == "" ||
+    req.body.password == "" ||
+    req.body.cpassword == ""
+  ) {
+    res.send({ msg: "Something Went Wrong ", success: false });
+  } else {
+    //Generating Salt and Hash for the password
+    const salt = await bcrypt.genSalt(10);
+    const securePassword = await bcrypt.hash(req.body.password, salt);
+
+    const saveData = new userSchema({
+      username: req.body.username,
+      email: req.body.password,
+      password: securePassword,
+    });
+    await saveData
+      .save()
+      .then(() => {
         console.log(saveData);
-    } else if (user) {
-        res.send({ msg: "Username Already Exist", sucess: false })
-        console.log("user is already exist")
-    } else if (email) {
-        res.send({ msg: "Email Already Exist", success: false })
-        console.log("email is already exist")
-    } else if (req.body.password !== req.body.cpassword) {
-        res.send("Password and Confirm Password must be same")
-    } else {
-        //Generating Salt and Hash for the password
-        const salt = await bcrypt.genSalt(10);
-        const securePassword = await bcrypt.hash(req.body.password, salt);
-        
-        const saveData = new userSchema({
-            username : req.body.username,
-            email : req.body.password,
-            password : securePassword,
-        });
-        await saveData.save().then(() => {
-            console.log(saveData);
-        }).catch((err) => {
-            console.log("error");
-        });
-        const data = {
-            user : saveData
-        }
-        const authtoken = jwt.sign(data, process.env.JWT_SECRET)
-        res.status(200).send({ msg: "Account Registered Succesfully",authtoken, success: true })
-    }
+      })
+      .catch((err) => {
+        console.log("error");
+      });
+    const data = {
+      user: saveData,
+    };
+    const authtoken = jwt.sign(data, process.env.JWT_SECRET);
+    res
+      .status(200)
+      .send({
+        msg: "Account Registered Succesfully",
+        authtoken,
+        success: true,
+      });
+  }
+});
 
-})
-
-module.exports = router
+module.exports = router;
